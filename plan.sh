f0feeff10100e72d1dbfd01e56417d69af58032a
Lucas Giancola <lucas.giancola@live.com>
2021-03-18 11:17:43 -0400

    Bumped version 1.19.2 -> 1.19.8

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nginx
 pkg_origin=core
-pkg_version=1.19.2
+pkg_version=1.19.8
 pkg_description="NGINX web server."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD-2-Clause')
 pkg_source="https://nginx.org/download/nginx-${pkg_version}.tar.gz"
 pkg_upstream_url=https://nginx.org/
-pkg_shasum=7c1f7bb13e79433ee930c597d272a64bc6e30c356a48524f38fd34fa88d62473
+pkg_shasum=308919b1a1359315a8066578472f998f14cb32af8de605a3743acca834348b05
 pkg_deps=(
   core/glibc
   core/libedit
